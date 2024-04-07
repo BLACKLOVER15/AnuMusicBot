bef8703608aad0a337595162aeb78edbe7e8aa1c
Black Lover <158266786+BLACKLOVER15@users.noreply.github.com>
2024-04-07 12:28:51 +0530

    Update userbot.py

--- LippsMusic/core/userbot.py
+++ LippsMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("ThinkGrowMore")
-                await self.one.join_chat("GJ516_DISCUSS_GROUP")
+                await self.one.join_chat("Exampur15")
+                await self.one.join_chat("All_Exam_Music_Support")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("ThinkGrowMore")
-                await self.two.join_chat("GJ516_DISCUSS_GROUP")
+                await self.two.join_chat("Exampur15")
+                await self.two.join_chat("All_Exam_Music_Support")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("ThinkGrowMore")
-                await self.three.join_chat("GJ516_DISCUSS_GROUP")
+                await self.three.join_chat("Exampur15")
+                await self.three.join_chat("All_Exam_Music_Support")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("ThinkGrowMore")
-                await self.four.join_chat("GJ516_DISCUSS_GROUP")
+                await self.four.join_chat("Exampur15")
+                await self.four.join_chat("All_Exam_Music_Support")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("ThinkGrowMore")
-                await self.five.join_chat("GJ516_DISCUSS_GROUP")
+                await self.five.join_chat("Exampur15")
+                await self.five.join_chat("All_Exam_Music_Support")
             except:
                 pass
             assistants.append(5)
